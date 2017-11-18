6fff4ab6ba3dd16b7912453210a0942c29861e57
Faris Mohammed <faris.mohammed@dstvdm.com>
2017-11-18 12:59:53 +0200

    adds create directory functionality

--- exekute.py
+++ exekute.py
@@ -1,5 +1,5 @@
 import click
-from os import remove
+from os import remove,mkdir
 from os.path import isfile
 from shutil import copy, copytree, rmtree
 
@@ -9,13 +9,19 @@ def copy_files(_from, _to):
 def remove_files(arg):
     remove(arg) if isfile(arg) else rmtree(arg)
 
+def create_directory(arg):
+    mkdir(arg)
+
 @click.command()
 @click.option('--copy', '-cp', multiple=True, nargs=2,help="The equivelent of the linux command cp.")
 @click.option('--remove', '-rm', multiple=True, nargs=1,help="The equivelent of the linux command rm.")
-def cli(copy, remove):
+@click.option('--mkdir', nargs=1,help="The equivelent of the linux command mkdir.")
+def cli(copy, remove, mkdir):
     if copy:
         _from = copy[0][0]
         _to = copy[0][1]
         copy_files(_from, _to)
     if remove:
         remove_files(remove[0])
+    if mkdir:
+        create_directory(mkdir)

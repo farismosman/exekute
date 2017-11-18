712d6ed86e4c9567be377dee27da329bfa6f0b1d
Faris Mohammed <faris.mohammed@dstvdm.com>
2017-11-18 13:46:53 +0200

    inlined helper methods

--- exekute.py
+++ exekute.py
@@ -1,16 +1,8 @@
 import click
-from os import remove,mkdir
+import os
 from os.path import isfile
 from shutil import copy, copytree, rmtree
 
-def copy_files(_from, _to):
-    copy(_from, _to) if isfile(_from) else copytree(_from, _to)
-
-def remove_files(arg):
-    remove(arg) if isfile(arg) else rmtree(arg)
-
-def create_directory(arg):
-    mkdir(arg)
 
 @click.command()
 @click.option('--copy', '-cp', multiple=True, nargs=2,help="The equivelent of the linux command cp.")
@@ -20,8 +12,8 @@ def cli(copy, remove, mkdir):
     if copy:
         _from = copy[0][0]
         _to = copy[0][1]
-        copy_files(_from, _to)
+        copy(_from, _to) if isfile(_from) else copytree(_from, _to)
     if remove:
-        remove_files(remove[0])
+        os.remove(remove[0]) if isfile(remove[0]) else rmtree(remove[0])
     if mkdir:
-        create_directory(mkdir)
+        os.mkdir(mkdir)

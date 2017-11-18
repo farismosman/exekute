97e09096d7830c3fa0b9831958d44dbe146ba46b
Faris Mohammed <faris.mohammed@dstvdm.com>
2017-11-18 10:12:41 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='exekute',
+    version='0.1',
+    py_modules=['exekute'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        exekute=exekute:cli
+    ''',
+)

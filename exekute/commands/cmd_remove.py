10c55317c45c088bbd23862b988a7522086353f2
Faris Mohammed <farismosman@gmail.com>
2017-11-19 22:25:26 +0200

    adds --yes option to remove command

--- exekute/commands/cmd_remove.py
+++ exekute/commands/cmd_remove.py
@@ -4,8 +4,15 @@ from os.path import isfile
 from complex.cli import pass_context
 
 
+def abort(ctx, opts, value):
+    if not value:
+        ctx.abort()
+
 @click.command('remove', short_help='')
 @click.argument('filename', nargs=1, required=True)
+@click.option('--yes', is_flag=True, callback=abort,
+              expose_value=False,
+              prompt='Are you sure you want to remove files?')
 @pass_context
 def cli(ctx, filename):
     os.remove(filename) if isfile(filename) else shutil.rmtree(filename)
